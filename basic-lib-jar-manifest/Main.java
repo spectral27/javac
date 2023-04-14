bbeded56bfdf0474b7d35123cbd84d5c1b477ac4
spectre2727 <gsoldano27@outlook.com>
2023-04-14 20:38:45 +0200

    javac

---
+++ basic-lib-jar-manifest/Main.java
@@ -0,0 +1,19 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Main {
+    public static void main(String[] args) {
+        JavaObject javaObject = new JavaObject();
+        javaObject.setVendor(System.getProperty("java.vendor"));
+        javaObject.setVersion(System.getProperty("java.version"));
+        ObjectMapper jackson = new ObjectMapper();
+        String json = new String();
+        try {
+            json = jackson.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(javaObject);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        System.out.println(json);
+    }
+}

a6fd5be4d9ca83a9f719bcc1a4c7720d6b274e38
spectre2727 <gsoldano27@outlook.com>
2023-04-18 10:58:25 +0200

    javac

---
+++ hibernate-6.2.1/src/data.sql
@@ -0,0 +1,5 @@
+create table javaobject(
+    vendor varchar(255),
+    version varchar(255),
+    primary key (vendor)
+);
---
+++ hibernate-6.2.1/src/spc/JavaObject.java
@@ -0,0 +1,24 @@
+package spc;
+
+public class JavaObject {
+    
+    private String vendor;
+    private String version;
+
+    public String getVendor() {
+        return this.vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public String getVersion() {
+        return this.version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+}

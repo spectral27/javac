a8ef1632270c3f653a3ee62464c56499b8816a3e
spectre2727 <gsoldano27@outlook.com>
2023-04-21 00:12:07 +0200

    javac

---
+++ frameworks/hibernate-xml-5.6.15/src/spc/Main.java
@@ -0,0 +1,61 @@
+package spc;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.h2.Driver");
+            DriverManager.getConnection(
+                "jdbc:h2:mem:db1;INIT=runscript from 'classpath:data.sql'",
+                "username",
+                "password"
+            );
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+        JavaObject java = new JavaObject();
+        java.setId(1);
+        java.setVendor(System.getProperty("java.vendor"));
+        java.setVersion(System.getProperty("java.version"));
+
+        JavaObject hibernate = new JavaObject();
+        hibernate.setId(2);
+        hibernate.setVendor("Hibernate Framework");
+        hibernate.setVersion("6.2.1");
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.persist(java);
+        session.persist(hibernate);
+        session.getTransaction().commit();
+        session.close();
+
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<JavaObject> javaObjects = session
+                .createQuery("select j from JavaObject j", JavaObject.class)
+                .list();
+        session.getTransaction().commit();
+        session.close();
+
+        for (JavaObject javaObject : javaObjects) {
+            System.out.printf("%s %s\n", javaObject.getVendor(), javaObject.getVersion());
+        }
+    }
+
+}
